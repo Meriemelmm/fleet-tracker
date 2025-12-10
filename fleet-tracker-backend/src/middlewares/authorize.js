305085cb61e841d9dfd857434db078179e607fd9
meriem el mecaniqui <melmecaniqui@gmail.com>
2025-12-10 22:43:40 +0100

    FLEET-51 authorizeMiddleware

---
+++ fleet-tracker-backend/src/middlewares/authorize.js
@@ -0,0 +1,15 @@
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      
+      return res.status(401).json({ message: "Non authentifié." });
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Accès refusé. Permissions insuffisantes." });
+    }
+
+    next(); 
+  };
+};
+export default authorize;
