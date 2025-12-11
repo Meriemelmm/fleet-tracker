3b625104b55861c775e16df5d15b7f03d91a4a55
meriem el mecaniqui <melmecaniqui@gmail.com>
2025-12-11 09:20:41 +0100

    update FLEET-50 authMiddleware

--- fleet-tracker-backend/src/middlewares/auth.js
+++ fleet-tracker-backend/src/middlewares/auth.js
@@ -1,45 +1,55 @@
-import User from '../models/User.js'
-const { verifyToken } = require('../utils/jwt.js');
+import User from "../models/User.js";
+import { verifyToken } from "../utils/jwt.js";
 
 const auth = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return res.status(401).json({
-                status: 'error',
-                message: 'Authorization header missing or malformed'
-            });
-        }
-        const bearerToken = authHeader.slice('')[1];
-        if (!bearerToken) {
-            return res.status(401).json({
-                status: 'error',
-                message: 'Token not provided'
-            });
-        }
+  try {
+    const authHeader = req.headers.authorization;
 
-        const decoded = verifyToken(bearerToken);
-        if (!decoded) {
-            return res.status(401).json({
-                status: 'error',
-                message: 'Invalid or expired token'
-            });
-        }
-
-        const user = await User.findById(decoded.id).select('-password');
-        if (!user) {
-            return res.status(401).json({
-                status: 'error',
-                message: 'User not found'
-            })
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-
-        next(error);
-
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        status: "error",
+        message: "Authorization header missing or malformed",
+      });
     }
-}
+
+  
+    const bearerToken = authHeader.split(" ")[1];
+
+    if (!bearerToken) {
+      return res.status(401).json({
+        status: "error",
+        message: "Token not provided",
+      });
+    }
+
+    const decoded = verifyToken(bearerToken);
+
+    if (!decoded) {
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid or expired token",
+      });
+    }
+
+   
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        message: "User not found",
+      });
+    }
+
+   
+    req.user = user;
+    req.token = bearerToken;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default auth;
